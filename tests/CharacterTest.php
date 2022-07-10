b050fc60871adcc77047f585ace46f2a72f0c678
Viviana <vivitr5195@gmail.com>
2022-07-11 01:28:00 +0200

    clone repo f5

---
+++ tests/CharacterTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\Character;
+use App\Faction;
+
+class CharacterTest extends TestCase {
+
+	/** @test */
+	public function test_function_example()
+	{
+		$this->assertEquals(4, 4);
+	}
+	
+
+}
+
+
