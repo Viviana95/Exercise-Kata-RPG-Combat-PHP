aeb8a4ae61d350594293791440c6837d6d234793
Viviana <vivitr5195@gmail.com>
2022-07-19 08:56:49 +0200

    add test

--- src/Character.php
+++ src/Character.php
@@ -5,13 +5,14 @@ namespace App;
 class Character {
 private int $health;
 private int $level;
-
+private bool $alive;
 
 
 public function __construct()
 {
     $this->health = 1000;
     $this->level = 1;
+    $this->alive = true;
    
    
 }
@@ -19,18 +20,45 @@ public function getHealth()
 {
     return $this->health;
 }
+public function setHealth($health)
+{
+    $this->health = $health;
+    return $this;
+}
 public function getLevel()
 {
     return $this->level;
 }
-public function getAlive()
+public function Alive()
 {
     if($this->health >= 1){
-        return "Alive";
+    $this->alive=true;
+    return $this->alive;
     }
-    return  "Dead";
+    return  $this->alive=false;
+}
+public function attack($enemy, $damage)
+{
+    $enemy->health=$enemy->health - $damage;
 }
 
+public function attackdamage($enemy,$damage ){
+    if ($enemy->health < $damage  ) {
+        $damage->health = 0;
+        return "Dead";
+        return $this->alive;
+}}
+
+public function heal($character, $healPoints){
+    $character->health = $character->health + $healPoints;
 }
 
+public function health($character,$healthto){
+if ($character->health == 0) return;
+if ($character->health + $healthto > 1000) {
+  $newlive = $character->health += $healthto;
+    return $newlive ;
+}  
+}
+}
 ?>
--- tests/CharacterTest.php
+++ tests/CharacterTest.php
@@ -4,7 +4,7 @@ namespace Tests;
 
 use PHPUnit\Framework\TestCase;
 use App\Character;
-
+use LDAP\Result;
 
 class CharacterTest extends TestCase {
 
@@ -24,12 +24,48 @@ class CharacterTest extends TestCase {
 	public function test_live()
 	{
 		$yoshi = new Character;
-		$yoshiLive = $yoshi ->getAlive();
-		$this->assertEquals("Alive", $yoshiLive);
+		$yoshiLive = $yoshi ->Alive();
+		$this->assertEquals(true, $yoshiLive);
 	}
+	public function test_character_damage()
+	{
+		$peach = new Character;
+		$toad = new Character;
+		
+		$peach->attack($toad,200);
+		$result = $toad->getHealth();
+		$this->assertEquals(800, $result);
+	}
+	public function test_character_damage_alive()
+	{
+		$peach = new Character;
+		$toad = new Character;
+		$peach->attackdamage($toad, 1000);
+		$result = $toad->getHealth();
+		$this->assertEquals(true, $result);
+		$this->assertEquals(false, $toad->Alive());
+	}
+	public function test_character__healtTo()
+	{
+		$mario = new Character;
+		$toad = new Character;
+		
+		$toad->setHealth(700);
+		$mario->heal($toad,300);
 	
-	
+		$this->assertEquals(1000, $toad->getHealth());
+		
+	}
 
+	public function test_character__cant_be_healed()
+	{
+		$mario = new Character;
+		$toad = new Character;
+		$healthTo = 100;
+		$result = $mario->health($toad, $healthTo);
+		$this->assertEquals(150, $result);
+		
+	}
 }
 
 
